3f3e9199e5bea232e2b1c4f21b8e38256ef3b25c
Frank <therealoneisneo@gmail.com>
2015-10-16 17:54:07 -0400

    Friday afternoon

---
+++ MLcodingAssignments/HW3/try.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+
+a = np.array(["love", "loving", "loves", "loved", "wonderful", "best", "great", "superb", "still", "beautiful", "bad", "worst", "stupid", "waste", "boring", "?", "!"])
+b = np.zeros(17)
+
+print a
+print b
