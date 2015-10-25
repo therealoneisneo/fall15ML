7e2253c80cdcfed45978b4726724db198e8001c1
therealoneisneo <therealoneisneo@gmail.com>
2015-10-25 02:20:18 -0400

    few bugs to fix and test

--- MLcodingAssignments/FinalProject/DataProcess.py
+++ MLcodingAssignments/FinalProject/DataProcess.py
@@ -40,21 +40,18 @@ class readLabel: # the class to read a label file
 
 		# the entries in catLabel are :
 		# 0: ground truth
-		# 1 - 6 : C - E[1 - 6] category evaluation
-		# 7: Female actor category evaluation
-		# 8: male actoor category evaluation
+		# middle part: evaluator category evaluations
+		# -1: actor category evaluation
 
-		# VADlabel = [] 
+		# VADLabel = [] 
 
 		# 1D vector of VAD values : val, act, dom
 		# 0 - 2 : ground truth
-		# 3 - 20 : A-E[1-6] VAD evaluation
-		# 21 - 23: Female actor VAD evaluation
-		# 23 - 25: Male actor VAD evaluation
-
+		# middle part : VAD evaluation of evaluators (all in 3 -tuples)
+		# -3 - -1: actor VAD evaluation
 		
-		labelMatrix = [] # label matrix
-		VADMatrix = [] # VAD matrix
+		labelMatrix = [] # label matrix, a list of ndarrays
+		VADMatrix = [] # VAD matrix a list of ndarrays
 
 		nameBegin = True # the begin indicator of the parsing of filenames
 
@@ -63,7 +60,7 @@ class readLabel: # the class to read a label file
 		VADMatrixBegin = True # the begin indicator of the parsing of VADMatrix
 
 		catLabel = []
-		VADlabel = []
+		VADLabel = []
 
 		while(1):
 			tempstring = inputfile.readline()
@@ -71,17 +68,25 @@ class readLabel: # the class to read a label file
 				break
 
 			tempstring = tempstring.strip()
-			tempstring = tempstring.split('\t')
+			
 
 			# catGroundTruth = "" # category ground truth buffer
+			
+			if tempstring[0] == '[': # indicate the start of a new instance. read in the filename and the ground truth category label
 
-			if tempstring[0][0] == '[': # read in the filename and the ground truth category label
+				tempstring = tempstring.split('\t')
+
+			# push the category label vector of previous instance into the matrix
+				if catLabel:
+					labelMatrix.append(catLabel)
+				if VADLabel:
+					VADMatrix.append(VADLabel)
 
 			# initiate
 				catLabelBegin = True
-				VADlabelBegin = True # the begin indicator of this specific instance
+				VADLabelBegin = True # the begin indicator of this specific instance
 				# catLabel = []
-				# VADlabel = []
+				# VADLabel = []
 			# the begin of an instance
 				
 				if nameBegin:
@@ -95,32 +100,33 @@ class readLabel: # the class to read a label file
 				vadParse = tempstring[3].strip('[')
 				vadParse = vadParse.strip(']')
 				vadParse = vadParse.split(', ')
-				VADlabel = np.asarray(float(vadParse[0]))
-				VADlabel = np.hstack((VADlabel, np.asarray(float(vadParse[1]))))
-				VADlabel = np.hstack((VADlabel, np.asarray(float(vadParse[2]))))
+				VADLabel = np.asarray(float(vadParse[0]))
+				VADLabel = np.hstack((VADLabel, np.asarray(float(vadParse[1]))))
+				VADLabel = np.hstack((VADLabel, np.asarray(float(vadParse[2]))))
 
 			else:
-				if tempstring[0][0] == 'C':
-					if tempstring[0][2] == 'E'
-						cat = tempstring[1].strip(';')
-						for i in range(1, 7):
-							if int(tempstring[0][3]) == i:
-								catLabel = np.hstack((catLabel, cat))
-							else:
-								catLabel = np.hstack((catLabel, "nan"))
-					if tempstring[0][2] == 'F':
+				if tempstring[0] == 'C': # A category label
+					tempstring = tempstring.split('\t')
+					tempCate = tempstring[1].split(' ')
+					for cate in tempCate:
+						cate = cate.strip(';')
+						catLabel = np.hstack((catLabel, cate))
 
-						
+				if tempstring[0] == 'A': # A VAD label
+					tempstring = tempstring.split('\t')
+					tempCate = tempstring[1].split(' ')
+					for i in (1,3,5):
+						VADLabel = np.hstack((VADLabel, tempstring[1][i].strip(';')))
 
-			elif labelMatrixBegin:
-				labelMatrix = catLabel
-				labelMatrixBegin = False
+			# elif labelMatrixBegin:
+			# 	labelMatrix = catLabel
+			# 	labelMatrixBegin = False
 
 		inputfile.close()
 
 		# print filenames
 		# print filelabels
-		return filenames, filelabels
+		return filenames, labelMatrix, VADMatrix
 
 	def labelVectorGen(self, rangeStart, rangeEnd):
 		gfileNames = [] #fileNames of all files
@@ -182,9 +188,10 @@ if __name__ == "__main__":
 	
 
 	test = readLabel()
-	name, label = test.readFile("Ses01F_impro01.txt")
+	name, label, vad = test.readFile("Ses01F_impro01.txt")
 	print name
 	print label
+	print vad
 
 
 		
--- MLcodingAssignments/FinalProject/try.py
+++ MLcodingAssignments/FinalProject/try.py
@@ -1,17 +1,30 @@
 import numpy as np
 import DataProcess as dp
 
-tempstring = "C-E2:	Neutral;	()"
-# print tempstring
+tempstring = "A-E4:	val 2; act 3; dom  3;	(guarded, tense, ready)"
+# # 
 tempstring = tempstring.strip()
 tempstring = tempstring.split('\t')
+
+print tempstring
+a = tempstring[1]
+a = a.split(' ')
+print a 
+
+for i in (1,3,5):
+	print i
+
+# for i in a:
+# 	i = i.strip(';')
+# 	print i
+# print tempstring[2][1]
 
 # parse = tempstring[3].strip('[')
 # parse = parse.strip(']')
 # parse = parse.split(', ')
 # print tempstring[0][0:3]
-if tempstring[0][2] == 'E':
-	print tempstring[0][3]
+# if tempstring[0][2] == 'E':
+# 	print tempstring[0][3]
 
 # cat = tempstring[1].strip(';')
 # print cat
@@ -19,3 +32,5 @@ if tempstring[0][2] == 'E':
 # print type(tempstring[3])
 # print parse
 # print type(parse[0])
+
+
