73da82eed7652affe2aa545d3eda257dd26d143b
therealoneisneo <therealoneisneo@gmail.com>
2015-10-24 21:41:12 -0400

    matrix proceccing

--- MLcodingAssignments/FinalProject/DataProcess.py
+++ MLcodingAssignments/FinalProject/DataProcess.py
@@ -35,17 +35,87 @@ class readLabel: # the class to read a label file
 
 		inputfile = open(self.filePath)
 
-		filenames = []
-		filelabels = []
+		filenames = [] # 1D vector of filenames
+		# catLabel = [] # 1D vector of category label
+
+		# the entries in catLabel are :
+		# 0: ground truth
+		# 1 - 6 : C - E[1 - 6] category evaluation
+		# 7: Female actor category evaluation
+		# 8: male actoor category evaluation
+
+		# VADlabel = [] 
+
+		# 1D vector of VAD values : val, act, dom
+		# 0 - 2 : ground truth
+		# 3 - 20 : A-E[1-6] VAD evaluation
+		# 21 - 23: Female actor VAD evaluation
+		# 23 - 25: Male actor VAD evaluation
+
+		
+		labelMatrix = [] # label matrix
+		VADMatrix = [] # VAD matrix
+
+		nameBegin = True # the begin indicator of the parsing of filenames
+
+		labelMatrixBegin = True # the begin indicator of the parsing of labelMatrix
+
+		VADMatrixBegin = True # the begin indicator of the parsing of VADMatrix
+
+		catLabel = []
+		VADlabel = []
+
 		while(1):
 			tempstring = inputfile.readline()
 			if not tempstring:
 				break
-				tempstring.strip()
-			if tempstring[0] == '[':
-				tempstring = tempstring.split('\t')
-				filenames.append(tempstring[1])
-				filelabels.append(tempstring[2])
+
+			tempstring = tempstring.strip()
+			tempstring = tempstring.split('\t')
+
+			# catGroundTruth = "" # category ground truth buffer
+
+			if tempstring[0][0] == '[': # read in the filename and the ground truth category label
+
+			# initiate
+				catLabelBegin = True
+				VADlabelBegin = True # the begin indicator of this specific instance
+				# catLabel = []
+				# VADlabel = []
+			# the begin of an instance
+				
+				if nameBegin:
+					filenames = np.asarray(tempstring[1])
+					nameBegin = False
+				else:
+					filenames = np.hstack((filenames, np.asarray(tempstring[1])))
+
+				catLabel = np.asarray(tempstring[2])
+
+				vadParse = tempstring[3].strip('[')
+				vadParse = vadParse.strip(']')
+				vadParse = vadParse.split(', ')
+				VADlabel = np.asarray(float(vadParse[0]))
+				VADlabel = np.hstack((VADlabel, np.asarray(float(vadParse[1]))))
+				VADlabel = np.hstack((VADlabel, np.asarray(float(vadParse[2]))))
+
+			else:
+				if tempstring[0][0] == 'C':
+					if tempstring[0][2] == 'E'
+						cat = tempstring[1].strip(';')
+						for i in range(1, 7):
+							if int(tempstring[0][3]) == i:
+								catLabel = np.hstack((catLabel, cat))
+							else:
+								catLabel = np.hstack((catLabel, "nan"))
+					if tempstring[0][2] == 'F':
+
+						
+
+			elif labelMatrixBegin:
+				labelMatrix = catLabel
+				labelMatrixBegin = False
+
 		inputfile.close()
 
 		# print filenames
@@ -93,11 +163,11 @@ class readLabel: # the class to read a label file
 
 			#finishe this after the formulation of label matrix
 
-		Rdata.saveToFile(fileNames, fileLabels)
+		Rdata.saveToFile(fileNames, fileLabels, "label_result.txt")
 		return
 
-	def saveToFile(self, fileNames, fileLabels):
-		outfile = open("label_result.txt", 'w')
+	def saveToFile(self, fileNames, fileLabels, targetFileName):
+		outfile = open(targetFileName, 'w')
 		num = len(fileNames)
 		for i in range(num):
 			temp = str(fileNames[i]) + '\t' + str(fileLabels[i]) + '\n'
--- MLcodingAssignments/FinalProject/try.py
+++ MLcodingAssignments/FinalProject/try.py
@@ -1,31 +1,21 @@
 import numpy as np
 import DataProcess as dp
 
-# path = "test.txt"
+tempstring = "C-E2:	Neutral;	()"
+# print tempstring
+tempstring = tempstring.strip()
+tempstring = tempstring.split('\t')
 
-# a = np.array([[1,2],[3,4]])
-# print a
-# a = a.flatten()
-# print a
+# parse = tempstring[3].strip('[')
+# parse = parse.strip(']')
+# parse = parse.split(', ')
+# print tempstring[0][0:3]
+if tempstring[0][2] == 'E':
+	print tempstring[0][3]
 
-# c = 1
-
-# a = "[10.0100 - 11.3925]	Ses01F_impro01_F001	neu	[2.5000, 2.5000, 2.5000]"
-
-# a = a.strip()
-
-# # for i in range(len(a)):
-# # 	if a[i] == '\t':
-# # 		print(1)
-# # 		print(a[i-1])
-# # 		a[i] = ' '
-# # 		print(2)
-# b = a.split('\t')
-
-
-
-# print b
-# # print a 
-
-# Rdata = dp.readLabel(path)
-# print Rdata.readFile()
+# cat = tempstring[1].strip(';')
+# print cat
+# print type(cat)
+# print type(tempstring[3])
+# print parse
+# print type(parse[0])
