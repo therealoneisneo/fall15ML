725790663b0aac190ae900cc210fd96735b82263
Frank <therealoneisneo@gmail.com>
2015-11-11 11:13:24 -0500

    grade ready

--- MLcodingAssignments/HW3/run.sh
+++ MLcodingAssignments/HW3/run.sh
@@ -1,5 +1,19 @@
 #! /bin/bash  
 
-# python number_recognition.py dtree zip.train zip.test
+python number_recognition.py dtree image_data/zip.train image_data/zip.test
 
-python naiveBayes.py data_sets data_sets/test_set
+
+python number_recognition.py knn image_data/zip.train image_data/zip.test
+
+python number_recognition.py net image_data/zip.train image_data/zip.test
+
+python number_recognition.py svm image_data/zip.train image_data/zip.test
+
+python number_recognition.py pcaknn image_data/zip.train image_data/zip.test
+
+python number_recognition.py pcasvm image_data/zip.train image_data/zip.test
+
+
+# python number_recognition.py svm image_data/zip.train image_data/zip.test
+
+# python naiveBayes.py data_sets data_sets/test_set
