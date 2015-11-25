83700a7c314652bd820f3c0d2bc82b510da4246f
Jinlong Feng <Frank@Jinlongs-MacBook-Air.local>
2015-11-25 07:37:22 -0500

    ready for training

--- MLcodingAssignments/FinalProject/Training.py
+++ MLcodingAssignments/FinalProject/Training.py
@@ -10,18 +10,16 @@ Tdata = dp.trainingData()
 
 # InstanceDic, trainX, trainy = Tdata.getTrainingData("train.data", False)
 
-Tdata.getTrainingData("train.data", True)
+Tdata.getTrainingData("train.data", True) # get data from train.data
 
-a,b,c,d = Tdata.leave1SesOut(1)
+trainX, trainy, testX, testy = Tdata.leave1SesOut(leaveOutSes = 1) # leave one session(couple) out, the argument is chose from 1 to 5
 
-# a,b,c,d = Tdata.male_female("F")
+trainX, trainy, testX, testy = Tdata.male_female(traingender = "F") # split female and male samples, "traingender" can be "F" or "M", which determines the content in trainX&trainy
 
-print a
-print b
-print c
-print d
+trainX, trainy, testX, testy = Tdata.controlCV(fold = 5) # the cross validation with session protion control, the argument "fold" in dicate the number of folds needed.
 
 
-# for key in Tdata.InstanceDic:
-# 	print len(Tdata.InstanceDic[key].featureVec)
+
+
+
 
